6aaa7c08b2f43ee38eb6bdb2b9c011fc61fd31b4
Christopher Nyberg <christopher.nyberg@gmail.com>
2016-08-14 20:41:46 -0700

    Awful performance but done
    Really need to figure out how to optimize.

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Data.List
 
+
 type Row = [ Maybe Int ]
 -- Todo limit int to 0-9
 
@@ -12,7 +13,86 @@ type Board = [Row]
 -- do not replace any existing data, only Nothing
 -- At each column, do not pick any numbers already used
 
--- replace the maybes with numbers
+solveSudoku :: Board -> [Board]
+solveSudoku board =
+  let
+    options = possibilitiesMatrix board
+    beastMatrix = sequence options
+  in
+    filter sudokuTest beastMatrix
+
+
+sudokuTest :: [Row] -> Bool
+sudokuTest matrix =
+  let
+    rowTest = all (\x -> fst x == 1) $ concatMap frequency matrix
+    colTest = all (\x -> fst x == 1) $ concatMap frequency $ transpose matrix
+  in
+    (rowTest && colTest)
+
+
+frequency :: Ord a => [a] -> [(Int,a)]
+frequency list = map (\l -> (length l, head l)) (group (sort list))
+
+
+possibilitiesMatrix :: Board -> [[Row]]
+possibilitiesMatrix board =
+  let
+    side = length board - 1
+  in
+    fmap (solutionFilter board) [0..side]
+
+
+solutionFilter :: Board -> Int ->[Row]
+solutionFilter board i =
+  filter (constraintChecker i board) (rowCombinator board i)
+
+
+constraintChecker :: Int -> Board -> Row -> Bool
+constraintChecker i initBoard row =
+  let
+    constraints = getColumnConstraints $ colConstraints i initBoard
+    intRow = takenSlots row
+    test = zip intRow constraints
+  in
+    all (\(x, y) -> notElem x y) test
+
+
+getColumnConstraints :: Board -> [[Int]]
+getColumnConstraints board =
+  takenSlots <$> transpose board
+
+
+-- This was a weird twist. You can't filter on constraints in your row
+-- Those are properties of the board itself and already resolved using
+-- Row combinator
+colConstraints :: Int -> Board -> Board
+colConstraints row board =
+  let
+    filterRows n board =
+      take n board ++ drop (n+1) board
+  in
+    filterRows row board
+
+
+rowCombinator :: Board -> Int -> [Row]
+rowCombinator board i =
+  let
+    row = board !! i
+  in
+    fmap (mergeRowAndUnusedList row) (allUnusedOptions row)
+
+
+mergeRowAndUnusedList :: Row -> [Int] -> Row
+mergeRowAndUnusedList = go []
+  where
+    go acc r l =
+      case (r, l) of
+        (Just r:rs, x:xs) -> go ( acc ++ [Just r] ) rs (x:xs)
+        (Nothing:rs, x:xs) -> go ( acc ++ [Just x] ) rs xs
+        (r:rs, []) -> go (acc ++ [r]) rs []
+        ([], _) -> acc
+
 
 allUnusedOptions :: Row -> [[Int]]
 allUnusedOptions row = permutations $ remainingOptions row
@@ -26,16 +106,8 @@ remainingOptions row =
     filter (`notElem` leftovers) [1..9]
 
 
-sampleRow :: Row
-sampleRow = [Just 5, Just 3, Nothing, Nothing, Just 7, Nothing, Nothing, Nothing, Nothing ]
-
-
 takenSlots :: Row -> [Int]
-takenSlots row = map maybePlucker $ filter (/= Nothing) row
-
-
-maybePlucker :: Maybe Int -> Int
-maybePlucker (Just a) = a
+takenSlots row = map (\ (Just a) -> a) $ filter (/= Nothing) row
 
 
 initBoard :: Board
@@ -53,4 +125,4 @@ initBoard
 
 
 main =
-  print initBoard
+  print $ solveSudoku initBoard
